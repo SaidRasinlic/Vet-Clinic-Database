d2e13e464feb567e83bb1145edc6dae99042721b
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-06-08 10:37:32 +0200

    Populate TABLE with DATA within DATABASE

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-/* Populate database with sample data. */
+/* PART I - Populate database with sample data. */
 
 INSERT INTO animals (id, name, date_of_birth, weight_kg, escape_attempts)
 VALUES (1, 'Agumon', '2020-11-03', 10.23, 0)
@@ -7,4 +7,28 @@ VALUES ('Gabumon', '2018-02-03', 8, 2, true)
 INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts)
 VALUES ('Pikachu', '2021-01-07', 15.04, 1)
 INSERT INTO animals (name, date_of_birth, weight_kg, escape_attempts, neutered)
+
+/* PART II */
+
 VALUES ('Devimon', '2017-05-12', 11, 5, true)
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Charmander', '2020-08-02', 11, 0, 'true' )
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Plantmon', '2021-11-15', 10.7, 2, 'true' )
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Squirtle', '1993-04-02', 12.13, 3, 'false' )
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Angemon', '2005-06-07', 20.4, 4, 'true' )
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Boarmon', '2005-06-07', 20.4, 7, 'true' )
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Blossom', '1998-10-13', 17, 3, 'true' )
+INSERT INTO animals (name, date_of_birth, weight_kg,
+					 escape_attempts, neutered)
+VALUES ('Ditto', '2022-05-14', 22, 4, 'true' )
